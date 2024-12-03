1a0f56ad07a9891826322b3c534d29a06068e536
Exiles.prx <exiles.prx@gmail.com>
2024-12-03 18:37:31 -0500

    Solved part two of day two

--- daytwo/puzzle.go
+++ daytwo/puzzle.go
@@ -3,7 +3,7 @@ package daytwo
 import (
 	"bufio"
 	"errors"
-	"log"
+	"fmt"
 	"os"
 	"sort"
 	"strconv"
@@ -13,45 +13,74 @@ import (
 func Puzzle(f string) (int, error) {
 	file, err := os.Open(f)
 	if err != nil {
-		log.Fatal("Error reading file")
+		fmt.Print("Error opening file")
 	}
 	defer file.Close()
 
 	safeReports := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		valid := true
-		previous := 0
 		text := strings.Split(scanner.Text(), " ")
-		numbers := []int{}
-		for _, r := range text {
-			n, err := strconv.Atoi(r)
-			if err != nil {
-				return 0, errors.New("Invalid number")
-			}
-			numbers = append(numbers, n)
+		numbers, err := convertToInt(text)
+		if err != nil {
+			return 0, err
 		}
 
-		iod := sort.IsSorted(sort.IntSlice(numbers)) || sort.IsSorted(sort.Reverse(sort.IntSlice(numbers)))
-		for _, n := range numbers {
-			if previous == 0 {
-				previous = n
-				continue
-			}
-
-			if !validDistance(previous - n) {
-				valid = false
-				break
-			}
-
-			previous = n
-		}
-
-		if valid && iod {
+		if validList(numbers) {
 			safeReports += 1
+		} else {
+			valid := false
+			for i := 0; i < len(numbers); i++ {
+				list := []int{}
+				list = append(append(list, numbers[:i]...), numbers[i+1:]...)
+				if validList(list) {
+					valid = true
+					break
+				}
+			}
+
+			if valid {
+				safeReports += 1
+			}
 		}
 	}
 	return safeReports, nil
+}
+
+func convertToInt(text []string) ([]int, error) {
+	numbers := []int{}
+	for _, r := range text {
+		n, err := strconv.Atoi(r)
+		if err != nil {
+			return nil, errors.New("Invalid number")
+		}
+		numbers = append(numbers, n)
+	}
+
+	return numbers, nil
+}
+
+func validList(numbers []int) bool {
+	iod := sort.IsSorted(sort.IntSlice(numbers)) || sort.IsSorted(sort.Reverse(sort.IntSlice(numbers)))
+	if !iod {
+		return false
+	}
+
+	previous := 0
+	for _, n := range numbers {
+		if previous == 0 {
+			previous = n
+			continue
+		}
+
+		if !validDistance(previous - n) {
+			return false
+		}
+
+		previous = n
+	}
+
+	return true
 }
 
 func validDistance(distance int) bool {
