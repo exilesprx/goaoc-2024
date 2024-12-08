6a549973e15d9d397d40ad0c134c7527d7b37757
Exiles.prx <exiles.prx@gmail.com>
2024-12-08 18:09:07 -0500

    Day six part one

---
+++ daysix/puzzle.go
@@ -0,0 +1,19 @@
+package daysix
+
+import (
+	"bufio"
+	"os"
+)
+
+func Puzzle(f string) (int, error) {
+	file, err := os.Open(f)
+	if err != nil {
+		return 0, err
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+	}
+
+	return 0, nil
+}
---
+++ daysix/puzzle_test.go
@@ -0,0 +1,26 @@
+package daysix
+
+import (
+	"testing"
+)
+
+func TestExample(t *testing.T) {
+	result, err := Puzzle("example.txt")
+	if err != nil {
+		t.Fatalf("Error: %v", err)
+	}
+
+	if result != 41 {
+		t.Fatalf("Expected 41, got %v", result)
+	}
+}
+
+func TestPuzzle(t *testing.T) {
+	t.Skip("Skipping test")
+	result, err := Puzzle("puzzle.txt")
+	if err != nil {
+		t.Fatalf("Error: %v", err)
+	}
+
+	t.Skipf("Result: %v", result)
+}
